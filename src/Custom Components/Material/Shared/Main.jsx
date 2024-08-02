a818327f3c5cc09824d970d4d77f8ee2a21364ea
manuo <ojhapranav1999@gmail.com>
2024-08-02 22:50:50 +0530

    first commiting all the files.

---
+++ src/Custom Components/Material/Shared/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+
+export const Main = () => {
+    return <>
+    <div> This is my header </div>
+    <div>This is my basic info part</div>
+    <div>My Journey</div>
+    <div>Projects</div>
+    <footer>My footer</footer>
+        </>
+}
