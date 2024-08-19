f7c8091b9a41a96d9612df21cfb64bdd5fb6778a
manuo <ojhapranav1999@gmail.com>
2024-08-19 23:23:27 +0530

    updating the footer with the latest info.

--- src/Home_page_components/Bottom/Footer.jsx
+++ src/Home_page_components/Bottom/Footer.jsx
@@ -5,10 +5,25 @@ import { BiArrowToTop } from "react-icons/bi";
 export const Footer = () => {
   return (<>
   <div className='footer_main_div'>
-    <span className='dancing_script_cursive'>Designed By - Pranav</span>
+    <div className='info_content'><h4>Let's connect :</h4> 
+      <span className='bright_content'><a href="mailto:ojhapranav1999@gmail.com" target='_blank'>ojhapranav1999@gmail.com</a></span>
+      <span className='bright_content'><a href='https://www.linkedin.com/in/pranav-ojha-14007a1a9' target='_blank'>Linked In</a></span>
+      <span className='bright_content'><a href='https://github.com/PranavJSX' target='_blank'>Github</a></span>
+
+
+
+      <a href='https://www.google.co.in/' target='_block'>Resume</a>
+      </div>
+    
     <button className='jump_to_top_button' onClick={()=>{document.querySelector('.top_nav')
     .scrollIntoView({behavior:'smooth'},true)}}><BiArrowToTop/></button>
+    
+    
+    
+    <span className='dancing_script_cursive'>Designed By - Pranav</span>
+    
   </div>
+
   </>
   )
 }
